26b9d927eddfcf7d238461dd9bad8b4b08bbcaff
league <maddoxseul@sbsd.net>
2024-01-25 19:56:50 -0800

    Weeks 4

---
+++ src/_04_netflix/_04_netflixrunner.java
@@ -0,0 +1,12 @@
+package _04_netflix;
+
+public class _04_netflixrunner {
+	public static void main(String[] args) {
+		public String getTicketPrice() {
+			if (this.stars > 2) {
+				return "That will be $12 please.";
+}
+		
+
+	}
+}
