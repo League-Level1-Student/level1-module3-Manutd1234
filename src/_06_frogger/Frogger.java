58bbfeac8164b5fd1d8f18cb48867b77b1882eba
Manutd1234 <105177593+Manutd1234@users.noreply.github.com>
2024-05-11 10:04:29 -0700

    sign out next time or I hack you

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -5,7 +5,17 @@ import processing.core.PApplet;
 public class Frogger extends PApplet {
     static final int WIDTH = 600;
     static final int HEIGHT = 400;
-    
+    //sign out of github next time
+    //sign out of github next time
+    //sign out of github next time
+    //sign out of github next time
+    //sign out of github next time
+    //sign out of github next time
+    //sign out of github next time
+    //sign out of github next time
+    //sign out of github next time
+    //sign out of github next time
+    //sign out of github next time
     @Override
     public void settings() {
         size(WIDTH, HEIGHT);
