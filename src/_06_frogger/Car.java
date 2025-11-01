749fb7a89627741313699929b7ccf8a8cd61a9e6
Aarit Jayaswal <aaritrj@gmail.com>
2025-11-01 11:27:29 -0700

    vyoufvjm

---
+++ src/_06_frogger/Car.java
@@ -0,0 +1,10 @@
+package _06_frogger;
+
+import java.security.DomainCombiner;
+
+public class Car {
+	int xPosition;
+	
+	Car bmw = new Car(int xPosition, int y position, size of car, speed of car);
+}
+
--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -3,24 +3,63 @@ package _06_frogger;
 import processing.core.PApplet;
 
 public class Frogger extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 400;
-    
-    @Override
-    public void settings() {
-        size(WIDTH, HEIGHT);
-    }
+	static final int WIDTH = 800;
+	static final int HEIGHT = 600;
+	int x = 400;
+	int y = 579;
 
-    @Override
-    public void setup() {
+	@Override
+	public void settings() {
+		size(WIDTH, HEIGHT);
+	}
 
-    }
+	@Override
+	public void setup() {
 
-    @Override
-    public void draw() {
+	}
 
-    }
-    static public void main(String[] args) {
-        PApplet.main(Frogger.class.getName());
-    }
+	@Override
+	public void draw() {
+		background(0, 0, 150);
+		fill(0, 20, 30);
+		ellipse(x, y, 30, 30);
+
+	}
+
+	static public void main(String[] args) {
+		PApplet.main(Frogger.class.getName());
+	}
+
+	public void keyPressed() {
+		if (key == CODED) {
+			// Frog Y position goes up
+			if (keyCode == UP) {
+				if(y > 20)
+				y -= 10;
+			}
+			// Frog Y position goes down
+			else if (keyCode == DOWN) {
+				if(y < 579) {
+					y += 10;
+				}
+				
+			}
+			// Frog X position goes right
+			else if (keyCode == RIGHT) {
+				if(x < 780) {
+				x += 10;	
+				}
+						
+			}
+
+			// Frog X position goes left
+			else if (keyCode == LEFT) {
+				if (x > 20) {
+					x -= 10;
+				}
+
+			}
+
+		}
+	}
 }
