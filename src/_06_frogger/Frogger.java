56aa23ddb9bd83f59fc1ebbdbdb50ac19b350a67
Aarit Jayaswal <aaritrj@gmail.com>
2025-11-05 19:55:56 -0800

    gvkhcgh,

--- src/_06_frogger/Frogger.java
+++ src/_06_frogger/Frogger.java
@@ -7,6 +7,10 @@ public class Frogger extends PApplet {
 	static final int HEIGHT = 600;
 	int x = 400;
 	int y = 579;
+	Car bmw;
+	Car porsche;
+	Car mustang;
+	Car ford;
 
 	@Override
 	public void settings() {
@@ -15,7 +19,10 @@ public class Frogger extends PApplet {
 
 	@Override
 	public void setup() {
-
+bmw = new Car(678,  545, 50, 3);
+porsche = new Car(500, 500, 50, 3);
+mustang =  new Car(700, 500, 50, 3);
+ford =new Car(600, 500, 50, 3);
 	}
 
 	@Override
@@ -23,8 +30,16 @@ public class Frogger extends PApplet {
 		background(0, 0, 150);
 		fill(0, 20, 30);
 		ellipse(x, y, 30, 30);
-
+bmw.display();
+porsche.display();
+mustang.display();
+ford.display();
+bmw.moveLeft();
+porsche.moveLeft();
+mustang.moveLeft();
+ford.moveRight();
 	}
+	
 
 	static public void main(String[] args) {
 		PApplet.main(Frogger.class.getName());
@@ -62,4 +77,35 @@ public class Frogger extends PApplet {
 
 		}
 	}
+	class Car{
+		int xPosition;
+		int yPosition;
+		int sizeOfCar;
+		int speedOfCar;
+		Car(int xPosition, int yPosition, int sizeOfCar, int speedOfCar){
+		this.xPosition= xPosition;
+		this.yPosition = yPosition;
+		this.sizeOfCar = sizeOfCar;
+		this.speedOfCar = speedOfCar;
+	}
+		
+	
+		void display()
+		  {
+		    fill(0,255,0);
+		    rect(xPosition , yPosition,  sizeOfCar, 50);
+		  }
+		void moveLeft() {
+			xPosition-=speedOfCar;
+			if(xPosition <= -sizeOfCar) {
+				xPosition = WIDTH;
+			}
+		}
+		void moveRight() {
+			xPosition+=speedOfCar;
+			if(xPosition >= 800) {
+				xPosition = -sizeOfCar;
+			}
+		}
+}
 }
